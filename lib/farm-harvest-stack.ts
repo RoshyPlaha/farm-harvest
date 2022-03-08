dd9ca98081b0d763df144a7309d8b45bd6fa99cb
Rosh Plaha <rosh.plaha@contino.io@Roshs-MBP.broadband>
2022-03-08 14:13:55 +0000

    cleanup

--- lib/farm-harvest-stack.ts
+++ lib/farm-harvest-stack.ts
@@ -1,13 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
-import { CodePipeline as codepipeline } from 'aws-cdk-lib/pipelines';
-import { CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipelineSource, ShellStep, CodePipeline as codepipeline } from 'aws-cdk-lib/pipelines';
 
 import { Construct } from 'constructs';
-import { stringify } from 'querystring';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FarmHarvestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -23,6 +17,6 @@ export class FarmHarvestStack extends Stack {
           'npx cdk synth'
         ]
       })
-    }
+    })
   }
 }
